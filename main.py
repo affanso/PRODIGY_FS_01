ba51e876f26d7242d5eb7d808258f39a3e44bde0
affanso <affansohail78@gmail.com>
2024-10-03 11:32:27 +0500

    Password Length Change

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class User(UserMixin,db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String(1000))
     email: Mapped[str] = mapped_column(String(100), unique=True)
-    password: Mapped[str] = mapped_column(String(500))
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
 
 with app.app_context():
     db.create_all()
